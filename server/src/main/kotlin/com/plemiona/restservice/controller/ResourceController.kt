fce3a39c89e04bc2db0b591cee27afdc258b9a2c
Kamil <tret1lus@gmail.com>
2020-06-05 01:29:50 +0200

    add army recruitment

--- server/src/main/kotlin/com/plemiona/restservice/controller/ResourceController.kt
+++ server/src/main/kotlin/com/plemiona/restservice/controller/ResourceController.kt
@@ -8,9 +8,12 @@ import com.plemiona.restservice.repos.PlayerRepository
 import com.plemiona.restservice.repos.VillageRepository
 import kotlinx.coroutines.flow.combineTransform
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("resources/")
@@ -30,6 +33,20 @@ class ResourceController(private val villageRepository: VillageRepository,
         }
         return mined
     }
+
+    @GetMapping("recruit/")
+    fun recruitArmy(playerid: Long, villageid: Long, amount: Int): HttpStatus {
+        var player = playerRepository.findById(playerid).get()
+        var village = villageRepository.findById(villageid).get()
+        return if (village.resources < amount*50) {
+            HttpStatus.BAD_REQUEST
+        } else {
+            village.resources -= amount*50
+            village.soldiers += amount
+            HttpStatus.ACCEPTED
+        }
+    }
+
 
 
 }
