b0664115ccf5f580a4096184432af321b5274779
68Kamil68 <tret1lus@gmail.com>
2020-06-09 19:38:19 +0200

    fix battle report

--- server/src/main/kotlin/com/plemiona/restservice/models/BattleReport.kt
+++ server/src/main/kotlin/com/plemiona/restservice/models/BattleReport.kt
@@ -6,10 +6,10 @@ import javax.persistence.*
 @Table(name="battlereport")
 data class BattleReport (
         @ManyToOne(fetch = FetchType.EAGER)
-        @JoinColumn(name = "village_id")
+        @JoinColumn(name = "village_id", referencedColumnName = "id", insertable =  false, updatable = false)
         val defenderVillage: Village? = null,
         @ManyToOne(fetch = FetchType.EAGER)
-        @JoinColumn(name = "village_id")
+        @JoinColumn(name = "village_id", referencedColumnName = "id", insertable =  false, updatable = false)
         val attackingVillage: Village? = null,
         val attackerArmy: Int = 0,
         val defenderArmy: Int = 0,
