23548edd9ac361e82a65beeb427c902dcdd26b23
JJackol <jacekczerwonka@gmail.com>
2020-06-09 22:58:10 +0200

    delete vuetify

--- front/src/main.js
+++ front/src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from "./plugins/axios";
-import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 axios.defaults.baseURL = "http://localhost:8080";
@@ -11,6 +10,5 @@ axios.defaults.baseURL = "http://localhost:8080";
 new Vue({
   router,
   store,
-  vuetify,
   render: h => h(App)
 }).$mount("#app");
