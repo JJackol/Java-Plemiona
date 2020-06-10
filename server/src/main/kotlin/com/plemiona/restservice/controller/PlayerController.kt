b315d3fe71d541b04fc782e06f0022f069011298
Kamil <tret1lus@gmail.com>
2020-06-10 09:35:31 +0200

    add requestParam addnotations to playerController

--- server/src/main/kotlin/com/plemiona/restservice/controller/PlayerController.kt
+++ server/src/main/kotlin/com/plemiona/restservice/controller/PlayerController.kt
@@ -1,23 +1,50 @@
 package com.plemiona.restservice.controller
 
+import com.plemiona.restservice.models.Player
 import com.plemiona.restservice.repos.PlayerRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/user")
-class PlayerController(private val repository: PlayerRepository) {
+class PlayerController(private val playerRepository: PlayerRepository) {
 
-    @GetMapping("/")
-    fun findAll() = repository.findAll()
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun registerUser (@RequestParam(value="username") username: String,
+                      @RequestParam(value="password") password: String,
+                      @RequestParam(value="email") email: String) :String{
+        var user = playerRepository.findByUsername(username)
+        return if (user == null) {
+            var player = Player(username = username, passwordHash = BCryptPasswordEncoder().encode(password), email = email)
+            playerRepository.save(player)
+            "User created"
+        }
+        else {
+            "User already exist"
+        }
+    }
 
-    @GetMapping("/{id}")
-    fun findOne(@PathVariable id: Long) =
-            repository.findByIdOrNull(id) ?: ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
-
-    @DeleteMapping("/{id}")
-    fun deleteOne(@PathVariable id: Long) =
-            repository.deleteById(id) ?: ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    fun loginUser (username: String, password: String) : Boolean {
+        var user = playerRepository.findByUsername(username)
+        if (user != null){
+            if (BCryptPasswordEncoder().matches(password, user.passwordHash)){
+                return true
+                "Welcome"
+            }
+            else{
+                return false
+                "Wrong password"
+            }
+        }
+        else{
+            return false
+            "Wrong username"
+        }
+    }
 }
